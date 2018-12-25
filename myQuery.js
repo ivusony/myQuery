cc9be57e7688a36e1a1b212ec5e65e5d053140bd
unknown <ivusony@gmail.com>
2018-12-25 10:10:20 +0100

    method flash

--- myQuery.js
+++ myQuery.js
@@ -1,21 +1,100 @@
+/*!
+ * myQuery JavaScript Library v1.0
+ * https://github.com/ivusony/myQuery
+ *
+ *
+ * Copyright Ivan Radulov
+
+ * Date: 2018-12-20
+ */
+
 (function(global){
- 
-    function methods(){
-        const temp = Object.create(HTMLCollection);
-        temp.eq = function(num){
-            const el = [this[num]];
-            Object.setPrototypeOf(el, this);
-            return el;
-        };
-        temp.last = function(){
-            return this.length
+
+    //adding size method to Object prototype makind object iterable
+    Object.prototype.size = function(){
+        let counter = 0;
+        for(let key in this){
+            if(this.hasOwnProperty(key)){
+                counter ++;
+            }
         }
-        temp.bg = function(color){
+        return counter
+    }
+
+    //return methods
+    function returnMethods(){
+        const method = Object.create(HTMLCollection);
+        //indexing methods
+        method.eq = function(num){
+                // console.log(this);
+                const el = [this[num]];
+                Object.setPrototypeOf(el, this);
+                return el;
+            }
+        method.last = function(){
+            return this.eq(this.size()-1)
+        }
+        
+        //styling methods
+        method.bg = function(color){
+            //check if no color is provided or is not string
+            if(!color || !typeof color==='string'){
+                //if so, set it to transparent
+                color = 'transparent'
+            }
            for(let key of Object.keys(this)){
                this[key].style.background = color;
            }
-        };
-        return temp;
+        }
+        method.delete = function(){
+            this.css('display','none')
+        }
+        method.undelete = function(){
+            this.css('display','block')
+        }
+        method.hide = function(){
+            this.css('visibility', 'hidden')
+        }
+        method.unhide = function(){
+            this.css('visibility', 'visible')
+        }
+        method.css = function(prop, value){
+            for(let key of Object.keys(this)){
+                this[key].style[prop]= value;
+            }
+        }
+        //animations
+        method.flash = function(freq){
+            const el = {
+                value   :   this,
+                visible  :   true,
+                freq    :   freq
+            };
+            //validate ease
+            //if not provided or not a number
+            if(!freq || !typeof freq==='Number'){
+                //set to 1000
+                el.freq = 1000
+            };
+            //animate
+            function animator(freq){
+                if(el.visible){
+                    setTimeout(function(){
+                        this.hide.call(el.value);
+                        el.visible=false;
+                        animator(el.freq);
+                    }.bind(method), freq)
+                }else{
+                    setTimeout(function(){
+                        this.unhide.call(el.value)
+                        el.visible=true;
+                        animator(el.freq);
+                    }.bind(method), freq)
+                }
+            }
+            animator.call(method, el.freq)
+        }
+        return method;
     }
 
     function nodeList(){
@@ -42,56 +121,62 @@
                         const el = document.getElementsByClassName(className);
                         // console.log(el);
                         return el;
+            },
+            NAME    : function(name){
+                        const el = document.getElementsByName(name);
+                        return el;
             }
         }
     })();
-        
-    
 
     //input sanitizer, determins the selector criteria
     function inputQuery(userQuery){
-       if(!String.prototype.trim){
-        // Make sure we trim BOM and NBSP
-        const rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-           String.prototype.trim = function(){
-            return this.replace(rtrim, '');
-           }
-       }
 
-       const query = userQuery.trim();
-
-       const elementMatched = (function(q){
+        if(!String.prototype.trim){
+            // Make sure we trim BOM and NBSP
+            const rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
+            //adding trim to string prototype
+            String.prototype.trim = function(){
+                return this.replace(rtrim, '');
+            }
+        }
+        
+        const query = userQuery.trim();
+        
+        //finding element
+        const elementMatched = (function(q){
             if(q[0]==='.'){
                 return matchSelector.CLASS(q.slice(1, q.length))
             }else if(q[0]==='#'){
                 return matchSelector.ID(q.slice(1, q.length));
+            }else if(q[0]==='*'){
+                return matchSelector.NAME(q.slice(1, q.length));
             }else{
                 return matchSelector.TAG(q)
             }
-       })(query); 
+        })(query); 
           
-       if(!elementMatched){
-           return new Error('No element found');
-       }else{
-           return elementMatched
-       }
-
-       
-
+        if(!elementMatched){
+            //if nothing found
+            return new Error('No element found');
+        }else{
+            //returning element found
+            return elementMatched
+        }
     } 
 
-    //get element
-    function matchElement(query, fn){
-        const selector = fn(); //the matchSelector function returning selector object
-        const el = selector.ID(query);
-        return el;
-    }
+    // //get element
+    // function matchElement(query, fn){
+    //     const selector = fn(); //the matchSelector function returning selector object
+    //     const el = selector.ID(query);
+    //     return el;
+    // }
 
 
 
     function factory(query){
         const el = inputQuery(query);
-        Object.setPrototypeOf(el, methods());
+        Object.setPrototypeOf(el, returnMethods());
        return el;
     }
 
