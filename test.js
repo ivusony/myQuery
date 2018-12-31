f18224177e5df3f4d51d2f2be10d7a0657707a07
radulko86 <radulko86@gmail.com>
2018-12-31 14:23:28 +0100

    first version

--- test.js
+++ test.js
@@ -1,46 +1,49 @@
-;(function(){
-    // const dragged = $('.applicant-container').drag();
+;(function(global){
+    // $('.applicant-container').drag()
 
-    // dragged.forEach(element => {
-    //     element.addEventListener('drag', function(e){
-    //         console.log('I am dragged')
-    //     })
-    // });
 
-    document.addEventListener('dragstart', function(e){
-        
-        e.preventDefault();
-        e.dataTransfer.effectAllowed = 'move';
-        e.dataTransfer.setData('text/plain', 'some_dummy_data');
-        var draggedElement = e.target;
-            draggedElement.style.opacity = 0.5;
-            console.log('dragstart');
-    },false)
 
-    document.addEventListener('dragover',function( e ) {
-        // prevent default to allow drop
-        e.preventDefault();
-    }, false);
+    var columns = document.getElementsByClassName('applicant-column');
+    var containers = document.getElementsByClassName('applicant-container');
+    var dragged;
 
+    for(let i=0; i<columns.size(); i++){
+        columns[i].addEventListener('dragover', function(e){
+            e.preventDefault();
+            // console.log(columns[i].id);
+            
+        })
+    }
+    for(let i=0;i<containers.size();i++){
+        containers[i].addEventListener('dragstart', function(e){
+            e.dataTransfer.setData('text/plain', 'dummy');
+            
+            dragged = e.target;
+
+            dragged.className += ' dragged'
+            dragged.style.opacity = 0.5;
+            // console.log(dragged);
+        })
+    }
+    for(let i=0;i<containers.size();i++){
+        document.addEventListener('dragend', function(e){
+            // console.log(e.target);
+            let currentClasses = dragged.className;
+        // console.log(currentClasses);
+        let newClasses = currentClasses.replace(' dragged', '');
+        dragged.className = newClasses += ' puff-in-center';
+        })
+    }
     document.addEventListener('drop', function(e){
         e.preventDefault();
-        console.log(e.target);
-        e.target.style.opacity = '';
-    },false)
+           
+        if(e.target.className === "column applicant-column"){
+
+        dragged.parentNode.removeChild( dragged );
+        e.target.appendChild( dragged );
+        dragged.style.opacity = 1;
+        }
+    })
     
-    const columns = document.getElementsByClassName('applicant-column');
-
-    for (let i = 0; i < columns.size(); i++) {
-        (function(index){
-            columns[index].addEventListener('mouseover', function(e){
-               
-            })
-        })(i)
-    }
-
-    // var draggedElement;
-
-    // document.addEventListener('drag', function(e){
-    //     console.log('draged');
-    // }, false)
-})();
+    
+})(window);
